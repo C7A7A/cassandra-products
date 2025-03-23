63eba38325e8fa7febeedf68329e9b8ceaacce46
Czaja <czajkamateusz999@gmail.com>
2025-03-23 16:37:13 +0100

    init

---
+++ src/main/java/io/github/c7a7a/cassandraproducts/CassandraProductsApplication.java
@@ -0,0 +1,13 @@
+package io.github.c7a7a.cassandraproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraProductsApplication.class, args);
+	}
+
+}
