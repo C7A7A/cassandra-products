127c0db80c77224a434683a7486335494f46d38d
Czaja <czajkamateusz999@gmail.com>
2025-03-24 12:40:05 +0100

    secure endpoint for admin (CREATE, UPDATE, DELETE)

--- backend/src/main/java/io/github/c7a7a/cassandraproducts/configuration/SecurityConfig.java
+++ backend/src/main/java/io/github/c7a7a/cassandraproducts/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.github.c7a7a.cassandraproducts.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/register", "/auth/login").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/products/*").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/products/*").hasRole("ADMIN")
+
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
